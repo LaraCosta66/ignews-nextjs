52c9953e1f9939170aedb304764f84dcc1ad125f
Lara Costa <larasmila64@gmail.com>
2023-06-05 12:36:06 -0300

    feat: create next app

---
+++ pages/_document.tsx
@@ -0,0 +1,19 @@
+import Document, { Head, Html, Main, NextScript } from "next/document";
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+           <link rel="preconnect" href="https://fonts.gstatic.com" />
+           <link href="https://fonts.googleapis.com/css2?family=Poppins&family=Roboto:wght@400;700;900&display=swap" rel="stylesheet"/>
+           <link rel="shortcut icon" href="/images/favicon.svg" type="image/svg" />  
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
