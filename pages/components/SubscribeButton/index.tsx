2e1abecf15602cf7177dd442c3b3d336b5a19eb5
Lara Costa <larasmila64@gmail.com>
2023-06-13 12:26:11 -0300

    feat: add checkout session

--- pages/components/SubscribeButton/index.tsx
+++ pages/components/SubscribeButton/index.tsx
@@ -1,3 +1,4 @@
+import { signIn, useSession } from "next-auth/react";
 import styles from"./styles.module.scss";
 
 interface SubscribeButton{
@@ -5,8 +6,17 @@ interface SubscribeButton{
 }
 
 export function SubscribeButton({priceId}:SubscribeButton){
+    const { data: session } = useSession();
+    function handleSubscribe(){
+        if(!session){
+            signIn('github')
+            return;
+        }
+        
+    }
     return(
-        <button type="button" className={styles.subscribeButton}>
+
+        <button type="button" className={styles.subscribeButton} onClick={handleSubscribe}>
             Subscribe Now
         </button>
     )
--- pages/services/stripe.ts
+++ pages/services/stripe.ts
@@ -9,3 +9,5 @@ export const stripe = new Stripe(
         version,
     },
 });
+
+
