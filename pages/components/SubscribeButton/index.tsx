1721617d6d616a5c0b430686b5bd616331f70026
Lara Costa <larasmila64@gmail.com>
2023-06-13 12:44:53 -0300

    feat: redirect user  to stipe payment method

--- pages/components/SubscribeButton/index.tsx
+++ pages/components/SubscribeButton/index.tsx
@@ -1,23 +1,36 @@
 import { signIn, useSession } from "next-auth/react";
-import styles from"./styles.module.scss";
+import styles from "./styles.module.scss";
+import { api } from "../../services/api";
+import { getStripeJs } from "../../services/stripe-js";
 
-interface SubscribeButton{
-    priceId: string;
+interface SubscribeButton {
+  priceId: string;
 }
 
-export function SubscribeButton({priceId}:SubscribeButton){
-    const { data: session } = useSession();
-    function handleSubscribe(){
-        if(!session){
-            signIn('github')
-            return;
-        }
-        
+export function SubscribeButton({ priceId }: SubscribeButton) {
+  const { data: session } = useSession();
+  async function handleSubscribe() {
+    if (!session) {
+      signIn("github");
+      return;
     }
-    return(
+    try {
+      const response = await api.post("/subscribe");
+      const { sessionId } = response.data;
+      const stripe = await getStripeJs();
 
-        <button type="button" className={styles.subscribeButton} onClick={handleSubscribe}>
-            Subscribe Now
-        </button>
-    )
+      await stripe?.redirectToCheckout({sessionId});
+    } catch (err) {
+      alert(err.message);
+    }
+  }
+  return (
+    <button
+      type="button"
+      className={styles.subscribeButton}
+      onClick={handleSubscribe}
+    >
+      Subscribe Now
+    </button>
+  );
 }
